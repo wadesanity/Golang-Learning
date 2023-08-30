75cc2ce9444d536bbd4d5a137479869e7f63f00a
wadesanity <624390633@qq.com>
2023-08-30 16:59:07 +0800

    完成,但是缺少分页请求所以就不并发了

--- 2.网络爬虫/case/wadesanity3_round2/main.go
+++ 2.网络爬虫/case/wadesanity3_round2/main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	logger.Logger.Println("start fuDa Spider")
 	fs := service.GetFuDaSpiderService()
-	fs.Do()
+	fs.Do() //缺少分页请求所以就不并发了
 	logger.Logger.Println("end fuDa Spider")
 }
